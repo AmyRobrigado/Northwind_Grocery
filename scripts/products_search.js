379a6dae0d3bf148d6605543ae86d32deaeb5dbd
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-12-11 19:06:40 -0500

    Created table, running into some issues with js

--- scripts/products_search.js
+++ scripts/products_search.js
@@ -1,22 +1,23 @@
 "use strict"
 
-window.onload = function(){
+window.onload = function () {
     multiSelectDropDown();
-    searchByCategory(); 
-    viewAllDropDown();
+    searchByCategory();
 
-    // Create hide & show functions 
-    // Create onchange events for the select 
-    // Make the tables for the information 
-    // Link the API to the dropdowns 
-
+    showCategorySearch();
+    hideCategorySearch();
+    hideMultiSelect(); 
+    showMultiSelect();
+    showTable(); 
+    hideTable();
+    
 
     document.getElementById("oneSlct").onchange = selectOneOnChange;
-    
+    document.getElementById("clearBtn").onclick = clearButton;
 }
 
-function multiSelectDropDown(){
-    const oneSelect = document.getElementById("oneSlct"); 
+function multiSelectDropDown() {
+    const oneSelect = document.getElementById("oneSlct");
     let multiOption = document.createElement("option");
     let categoryOption = document.createElement("option");
     let selectOneOption = document.createElement("option");
@@ -25,35 +26,110 @@ function multiSelectDropDown(){
     multiOption.value = "";
     selectOneOption.textContent = "Select One...";
     categoryOption.textContent = "Search by Category";
-    multiOption.textContent = "View All"; 
+    multiOption.textContent = "View All";
 
     oneSelect.appendChild(selectOneOption);
     oneSelect.appendChild(categoryOption);
     oneSelect.appendChild(multiOption);
 }
 
-function searchByCategory(){
-    const categorySelect = document.getElementById("categorySlct"); 
-    let categoryDrop = document.createElement("option"); 
+function searchByCategory() {
+    const categorySelect = document.getElementById("categorySlct");
+    let categoryDrop = document.createElement("option");
 
     categoryDrop.value = "";
-    categoryDrop.textContent = "Search by Category"; 
-
+    categoryDrop.textContent = "Select a Category";
 
     categorySelect.appendChild(categoryDrop);
+
+    fetch("http://localhost:8081/api/categories")
+        .then(response => response.json())
+        .then(categories => {
+            let categoriesLength = categories.length;
+            for (let i = 0; i < categoriesLength; i++) {
+                let catOptions = document.createElement("option");
+                catOptions.value = categories[i];
+                catOptions.textContent = categories[i].name;
+
+                categorySelect.appendChild(catOptions);
+            }
+        })
 }
 
-function viewAllDropDown(){
-    const viewAllDrop = document.getElementById("viewAll"); 
-    let viewDropDown = document.createElement("option"); 
-
-    viewDropDown.value = "";
-    viewDropDown.textContent = "View All"; 
-
-
-    viewAllDrop.appendChild(viewDropDown);
+function showCategorySearch() {
+    document.getElementById("categoryContainer").style.display = "block";
 }
 
-function selectOneOnChange(){
+function hideCategorySearch() {
+    document.getElementById("categoryContainer").style.display = "none";
+}
 
+function showMultiSelect() {
+    document.getElementById("oneSlct").style.display = "block";
+}
+
+function hideMultiSelect() {
+    document.getElementById("oneSlct").style.display = "none";
+}
+
+function showTable(){
+    document.getElementById("tableContainer").style.display = "block"; 
+}
+
+function hideTable(){
+    document.getElementById("tableContainer").style.display = "none"; 
+}
+
+function selectOneOnChange() {
+    let multiCategory = document.getElementById("oneSlct").value;
+    if (multiCategory == "Search by Category"){
+        showCategorySearch();
+        hideMultiSelect();  
+        hideTable();
+    }
+
+    else (multiCategory == "View All");{
+    const tableBody = document.getElementById("tbody");
+
+    fetch("http://localhost:8081/api/products")
+        .then(response => response.json())
+        .then(products => {
+            for (let p in products) {
+                let tableRow = document.createElement("tr");
+
+                tableBody.appendChild(tableRow);
+                //-----------------------------------
+
+                let productName = document.createElement("td");
+                productName.innerHTML = products[p].productName;
+                tableRow.appendChild(productName);
+
+                let unitPrice = document.createElement("td");
+                unitPrice.innerHTML = products[p].unitPrice;
+                tableRow.appendChild(unitPrice);
+
+                let inStock = document.createElement("td");
+                inStock.innerHTML = products[p].unitsInStock;
+                tableRow.appendChild(inStock);
+
+                let supplier = document.createElement("td");
+                supplier.innerHTML = products[p].supplier;
+                tableRow.appendChild(supplier);
+
+                let discontinue = document.createElement("td");
+                discontinue.innerHTML = products[p].discontinued;
+                tableRow.appendChild(discontinue);
+
+                showTable();
+            }
+        })
+    }
+    
+}
+
+function clearButton() {
+    document.getElementById("oneSlct").value = "Select One...";
+    showMultiSelect();
+    hideCategorySearch();
+    hideTable();
 }
